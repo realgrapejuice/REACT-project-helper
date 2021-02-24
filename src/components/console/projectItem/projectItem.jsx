a7ebaf6b370c9c92e6d4a29563ad01a10082402d
Realgrapejuice <66715905+realgrapejuice@users.noreply.github.com>
2021-02-24 23:23:44 +0900

    make delete project feature

--- src/components/console/projectItem/projectItem.jsx
+++ src/components/console/projectItem/projectItem.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import styles from "./projectItem.module.css";
 
-const ProjectItem = ({ item }) => (
-  <li className={styles.projectItem}>
-    <div className={styles.projectBox}>
-      <h1>{item.projectName}</h1>
-      <span>{`Created on ${item.date}`}</span>
-      <div className={styles.icon}>
-        <i className="far fa-folder"></i>
+const ProjectItem = ({ item }) => {
+  return (
+    <li className={styles.projectItem}>
+      <div className={styles.projectBox}>
+        <h1>{item.projectName}</h1>
+        <span className={styles.date}>{`Created on ${item.date}`}</span>
+        <div className={styles.modal}>
+          <i class="fas fa-times"></i>
+        </div>
+        <div className={styles.icon}>
+          <i className="far fa-folder"></i>
+        </div>
       </div>
-    </div>
-  </li>
-);
+    </li>
+  );
+};
 
 export default ProjectItem;
